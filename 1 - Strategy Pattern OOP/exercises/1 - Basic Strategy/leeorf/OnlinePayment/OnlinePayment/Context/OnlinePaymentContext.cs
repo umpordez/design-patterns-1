836741008012abb6150b9bc0db651962f29cd2df
Leo Rodrigues <leo.rf.dev@gmail.com>
2021-12-07 23:18:13 -0300

    feat: add solution of Strategy Pattern exercise 1

---
+++ 1 - Strategy Pattern OOP/exercises/1 - Basic Strategy/leeorf/OnlinePayment/OnlinePayment/Context/OnlinePaymentContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OnlinePayment.Interface;
+
+namespace OnlinePayment.Context
+{
+    public class OnlinePaymentContext
+    {
+        private OnlinePaymentStrategy _strategy;
+
+        public OnlinePaymentContext(OnlinePaymentStrategy onlinePaymentStrategy)
+        {
+            this._strategy = onlinePaymentStrategy;
+        }
+
+        public void executePayment()
+        {
+            this._strategy.Pay();
+        }
+    }
+}
---
+++ 1 - Strategy Pattern OOP/exercises/1 - Basic Strategy/leeorf/OnlinePayment/OnlinePayment/Operations/PayPal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OnlinePayment.Interface;
+
+namespace OnlinePayment.Operations
+{
+    public class PayPal : OnlinePaymentStrategy
+    {
+        public PayPal()
+        {
+        }
+
+        public void Pay()
+        {
+            Console.WriteLine("Calling PayPal api...");
+        }
+    }
+}
