02aacf28d9a0b0b2893df22525c5ff9884507f8e
Deividy Metheler Zachetti <deividyz@gmail.com>
2021-12-08 08:46:05 -0300

    adds clean exercise 1 in .NET

---
+++ 1 - Strategy Pattern OOP/exercises/1 - Basic Strategy/deividy/OnlinePayment/OnlinePayment/Operations/PicPay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OnlinePayment.Interface;
+
+namespace OnlinePayment.Operations
+{
+    public class PicPay : OnlinePaymentStrategy
+    {
+        public PicPay()
+        {
+        }
+
+        public void Pay()
+        {
+            Console.WriteLine("Calling PicPay api...");
+        }
+    }
+}
