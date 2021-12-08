8995cae21411af53fdb832a8c172a6cd1f947af7
Leo Rodrigues <leo.rf.dev@gmail.com>
2021-12-08 00:27:44 -0300

    feat: add solution of Strategy Pattern exercise 1

---
+++ 1 - Strategy Pattern OOP/exercises/1 - Basic Strategy/leeorf/OnlinePayment/OnlinePayment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OnlinePayment.Context;
+using OnlinePayment.Operations;
+
+namespace OnlinePayment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Client 1: I want to pay with PayPal!");
+            var payPalPayment = new OnlinePaymentContext(new PayPal());
+            payPalPayment.executePayment();
+
+            Console.WriteLine("Client 2: I want to pay with Mercado Pago!");
+            var mercadoPagoPayment = new OnlinePaymentContext(new MercadoPago());
+            mercadoPagoPayment.executePayment();
+
+            Console.WriteLine("Client 3: I want to pay with PicPay!");
+            var picPayPayment = new OnlinePaymentContext(new PicPay());
+            picPayPayment.executePayment();
+        }
+    }
+}
+
