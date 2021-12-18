9dbe9ba83c4d8e103719baca72b88b11d83602c2
Vanderson Rodrigues <vanderson@tutamail.com>
2021-12-18 14:31:19 -0300

    Add solution of Observer Pattern exercise 1

---
+++ 3 - Observer Pattern/exercises/1 - Node dom/vandito/node-dom.js
@@ -0,0 +1,34 @@
+class Dom {
+    _listeners = {};
+
+    addEventListener(eventName, handler) {
+        if (!this._listeners[eventName]) {
+            this._listeners[eventName] = [ ];
+        }
+
+        this._listeners[eventName].push(handler);
+    }
+
+    trigger(eventName, ...values) {
+        let events =  this._listeners[eventName];
+        for (let event of events) {
+            event(...values);
+        }
+    }
+}
+
+const dom = new Dom();
+
+dom.addEventListener('click', (ev) => {
+    console.log(`Click on ${ev.target}!`);
+});
+dom.addEventListener('hover', (ev) => {
+    console.log(`Hover on: ${ev.target}!`);
+});
+
+dom.addEventListener('click', (ev) => {
+    console.log(`Click but, this is another action for: ${ev.target}!`);
+});
+
+dom.trigger('click', { target: 'button' });
+dom.trigger('hover', { target: 'div' });
